5d151b2467dae84319577a0d862720694035c1a2
Mix4Pro <n.nishanbaev06@gmail.com>
2023-01-23 14:59:22 +0500

    First COmmit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Chat from "./components/Chat"
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Registration from './components/Registration';
 const socket = io.connect("https://chatbeta.onrender.com");
-
+console.log(socket)
 function App() {
   return (
     <>
