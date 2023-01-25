a0884d7ddb86d42afb1608c676cc5ef188842714
Mix4Pro <n.nishanbaev06@gmail.com>
2023-01-25 19:04:06 +0500

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import io from "socket.io-client"
 import Login from './components/Login';
 import Chat from "./components/Chat"
+import axios from 'axios';
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Registration from './components/Registration';
+import { useEffect } from 'react';
 const socket = io.connect("https://chatbeta.onrender.com");
+
+useEffect(()=>{
+  let response = await axios.get('https://chatbeta.onrender.com')
+
+  console.log(response)
+})
 function App() {
   return (
     <>
