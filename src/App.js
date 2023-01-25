b0c3b856bf78b30074f61987a09f0793c99a2b0f
Mix4Pro <n.nishanbaev06@gmail.com>
2023-01-25 19:12:30 +0500

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Registration from './components/Registration';
 import { useEffect } from 'react';
 const socket = io.connect("https://chatbeta.onrender.com");
-
-useEffect(()=>{
-  let response = await axios.get('https://chatbeta.onrender.com')
-
-  console.log(response)
-})
 function App() {
+  useEffect(()=>{
+    axios.get('https://chatbeta.onrender.com').then((data)=>{
+      console.log(data)
+    })
+  })
   return (
     <>
     <div className='container'>
@@ -31,9 +30,8 @@ function App() {
         </Routes>
       </Router>
     </div>
-      
     </>
-  );
+  );  
 }
 
 export default App;
