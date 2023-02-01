737ca820abce27a910992f50cbb263931aedcdcc
Mix4Pro <n.nishanbaev06@gmail.com>
2023-02-01 14:55:36 +0500

    First commit

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,125 +1,14 @@
-// import { useEffect,useState , useMemo } from 'react'
 // import { useLocation } from 'react-router-dom'
-import ReactScrollableFeed from "react-scrollable-feed"
-import axios from "axios"
-// function Chat({socket}) {
-//     let username
-//     console.log(username)
-//     const [message,setMessage] = useState('')
-//     const [messages, setMesasges] = useState([]) 
+import React, { Component} from 'react';
+import ReactScrollableFeed from "react-scrollable-feed";
+import axios from "axios";
 
-//     useMemo(()=>{
-//         axios.get('http://localhost:3001/chat')
-//         .then((data)=>{
-//             console.log(data.data)
-//             username = data.data    
-//             console.log(username)        
-//         })
-//     })
-
-//     const sendMessage = ()=>{
-//         if(message !== ''){
-//             const CurrentMessage = {
-//                 author: username,
-//                 message: message,
-//                 date: new Date(Date.now()).getHours() +
-//                 ":" +
-//                 new Date(Date.now()).getMinutes(),
-//             }
-//             socket.emit('send_message', CurrentMessage)
-//             setMesasges([...messages,CurrentMessage])
-//             setMessage('')
-//         }
-//     }
-    
-//     useEffect(()=>{
-//         socket.on('recieve_message', (data)=>{
-//             setMesasges([...messages,data])
-//         })
-//     })
-//   return (
-//     <div className='main-chatting'>
-//         <div className='main-contacts'>
-//             <div className='contacts-header'>
-//                 <p>Contacts</p>
-//             </div>
-//             <div className='contacts'>
-//                 <p className='contacts-name'>Beta Version XD</p>
-//             </div>
-//         </div>
-//         {
-//             username !== '' || username !== undefined ? (
-//                 <div className='chatting-div'>
-//                     <div className='message-header'>
-//                         <p>Username: {username}</p>
-//                     </div>
-
-//                     <div className='message-block'>
-//                         <ReactScrollableFeed>
-//                             {
-//                                 messages.map((val,i)=>{
-//                                     return (
-//                                         <div className='message' key={i}>
-//                                             <div className='message-author'>
-//                                                 <p>{val.author}</p>
-//                                             </div>
-//                                             <div className='message-p'  id={
-//                                                 username === val.author ? 'me' : 'another'
-//                                             }>
-//                                                 <p>{val.message}</p>
-//                                             </div>
-
-//                                             <div className='message-date'>
-//                                                 <p>{val.date}</p>
-//                                             </div>
-//                                         </div>
-//                                     )
-//                                 })
-//                             }
-//                         </ReactScrollableFeed>
-//                     </div>
-
-//                     <div className='message-form'>
-//                         <div className='ins-message-form'>
-//                             <textarea  
-//                                 placeholder='Message...' 
-//                                 rows="1"
-//                                 onChange={(e)=>{setMessage(e.target.value)}} 
-//                                 onKeyDown={(e)=>{
-//                                     if(e.key === "Enter" && !e.shiftKey){
-//                                         e.preventDefault()
-//                                         sendMessage()
-//                                         e.target.value = ''
-//                                     }
-//                                 }}
-//                             ></textarea>
-
-//                             <button onClick={sendMessage}>&#9658;</button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             ) : (
-//                 <div>
-//                     <h1>You are not logged</h1>
-//                 </div>
-//             )
-//         }
-//     </div>
-//   )
-// }
-
-
-
-// export default Chat
-
-
-import React, { Component } from 'react';
 
 class Chat extends Component {
     constructor(props){
         super(props);
-        
-        this.sendMessage = this.sendMessage.bind(this)
+        this.sendMessage = this.sendMessage.bind(this);
+        this.inputField = React.createRef();
     }
 
     state = {
@@ -138,6 +27,7 @@ class Chat extends Component {
                     ":" +
                     new Date(Date.now()).getMinutes(),
                }
+               this.inputField.current.value = ""
                let {author,message,date} = CurrentMessage
                await axios.post('https://chatbeta.onrender.com/chat-insert-message', {
                 author,message,date   
@@ -160,9 +50,15 @@ class Chat extends Component {
 
     async componentWillUnmount(){
         try{
+
+            console.log("Getting username")
             let userNameFromData = await axios.get("https://chatbeta.onrender.com/chat-get-username")
+
+            console.log("Username is gotten")
             
             let messagesFromData = await axios.get('https://chatbeta.onrender.com/chat-get-messages')
+
+            console.log("messages are gotten")
 
             console.log(messagesFromData)
             this.setState({
@@ -238,12 +134,13 @@ class Chat extends Component {
                                             if(e.key === "Enter" && !e.shiftKey){
                                                 e.preventDefault()
                                                 this.sendMessage()
-                                                e.target.value = ''
                                             }
                                         }}
+
+                                        ref={this.inputField}
                                     ></textarea>
 
-                                    <button onClick={this.sendMessage}>&#9658;</button>
+                                    <button type="submit" onClick={this.sendMessage}>&#9658;</button>
                                 </div>
                             </div>
                         </div>
