f007b4b548dfbc68c7fcba61adc9085f3a5a0d4f
Mix4Pro <n.nishanbaev06@gmail.com>
2023-02-09 14:23:17 +0500

    Second Commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,77 +1,141 @@
-import React, { Fragment, useState } from 'react'
-// import {Link} from "react-router-dom"
+import React, { Component } from 'react';
 import axios from "axios"
 import {Link} from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import {ToastContainer,toast} from "react-toastify"
+import { FaMoon } from 'react-icons/fa';
 import 'react-toastify/dist/ReactToastify.css';
 
-function Login({socket}) {
-  const navigate = useNavigate()
-  const [username,setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const toastOptions = {
-    className: "toast",
-    position: 'bottom-right',
-    autoClose: 8000,
-    pauseOnHover: true,
-    draggable: true,
+
+
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.onSubmitForm = this.onSubmitForm.bind(this)
+    this.changeTheme = this.changeTheme.bind(this)
+
+    this.inputs = React.createRef();
   }
-    const onSubmitForm = async (e)=>{
-      try{
-        console.log(socket)
-        if(username !== '' && password !== ''){
-          await socket.emit("login", username)
 
-          console.log("POST IS SENDING")
-          
-          await axios.post('https://chatbeta.onrender.com' , {
-            username , password
-          })
+  state = {
+    username: '',
+    password: '',
+    theme: false,
+    toastOptions: {
+      className: "toast",
+      position: 'bottom-right',
+      autoClose: 8000,
+      pauseOnHover: true,
+      draggable: true,
+    }
+  }
 
-          console.log("POST IS SENT")
+  async onSubmitForm(e){
+    try{
+      let username = this.state.username
+      let password = this.state.password
+      console.log(this.props.socket)
+      if(username !== '' && password !== ''){
+        await this.props.socket.emit("login",username)
 
-          localStorage.setItem("username",username)
+        console.log("POST IS SENDING")
+        
+        await axios.post('https://chatbeta.onrender.com' , {
+          username , password
+        })
 
-          // navigate('/chat',{
-          //   username: username
-          // })
+        console.log("POST IS SENT")
 
-          navigate('/chat')
-        }
-        }catch(e){
-          console.log(e)
-          toast.error("Username or Password is incorrect.", toastOptions)
-        }
-        e.preventDefault()
+        localStorage.setItem("username",username)
+
+        // navigate('/chat',{
+        //   username: username
+        // })
+
+        // navigate('/chat')
       }
-  return (
-    <Fragment>
-      <div className='div-login-form'>
-        <div className='login-form'>
-          <h1>Welcome to HoYo Chatting !</h1>
-          <form action='/' method='POST' onSubmit={()=> onSubmitForm}>
-            <input 
-              type='text' 
-              placeholder="Username..." 
-              onChange={(e)=>{setUsername(e.target.value)}}
-            />
+      }catch(e){
+        console.log(e)
+        toast.error("Username or Password is incorrect.", this.state.toastOptions)
+      }
+      e.preventDefault()
+    }
 
-            <input 
-              type='password' 
-              placeholder="Password..." 
-              onChange={(e)=>{setPassword(e.target.value)}}
-            />
-            <Link type='button' onClick={onSubmitForm} username={username}>Log in</Link>
+    async changeTheme(){
+      await this.setState({
+        theme: !this.state.theme
+      })
+      if(this.state.theme === true){
+        document.body.style.background = '#fff'
+        console.log('white')
+      }else{
+        document.body.style.background = '#060A13'
+        console.log('dark')
+      }
+    }
 
-          </form>
-          <Link to="/registration">Sign Up</Link>
+  
+  
+  componentWillMount(){
+    if(this.state.theme === true){
+      document.body.style.background = '#fff'
+      console.log('white')
+    }else{
+      document.body.style.background = '#060A13'
+      console.log('dark')
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={
+        this.state.theme ? 'main-login-div light' : 'main-login-div dark'
+      }>
+        <div className='login-header'>
+          <div className='theme-div'>
+            <button className='theme' onClick={this.changeTheme}><FaMoon style={{color: this.state.theme ? '#000' : '#fff'}} /></button>
+          </div>
+
+            <h2>Welcome To HoYo Chat!</h2>
+
+            <Link className='btn-sign-up' to='/registration'>Sign Up</Link>
         </div>
-      <ToastContainer />
+
+        <div className='login-form-div'>
+          <div className='login-form-inner-div'>
+            <h2>Log In</h2>
+            <form className='form' action='/' method='POST' onSubmit={()=> this.onSubmitForm}>
+              <input 
+                type='text' 
+                ref={this.inputs}
+                placeholder="Username..." 
+                className='login-form-inp'
+                onChange={(e)=>{
+                  this.setState({
+                    username: e.target.val
+                  })
+                }}
+              />
+
+              <input 
+                type='password' 
+                placeholder="Password..." 
+                className='login-form-inp'
+                onChange={(e)=>{
+                  this.setState({
+                    password: e.target.val
+                  })
+                }}
+              />
+            </form>
+            <Link className='btn-form' type='button' to={'/chat'} onClick={this.onSubmitForm} username={this.state.username}>Log in</Link>
+          </div>
+        </div>
+        <ToastContainer />
       </div>
-    </Fragment>
-  )
+    );
+  }
 }
 
-
-export default Login
+export default Login;
