90178a727a0c07277edcaed14e9d12db0f8e2fba
Mix4Pro <n.nishanbaev06@gmail.com>
2024-09-16 17:31:59 +0500

    Add existing project files to Git

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -72,7 +72,7 @@ class Chat extends Component {
                 })
                 console.log('GOING...')
                 console.log(image)
-                await axios.post('http://localhost:3001/chat-insert-message', {  
+                await axios.post('https://chatbeta.onrender.com/chat-insert-message', {  
                     author,message,date,image
                 })
                 console.log(CurrentMessage)
@@ -132,7 +132,7 @@ class Chat extends Component {
 
             console.log(typeof this.state.username) 
             
-            let messagesFromData = await axios.get('http://localhost:3001/chat-get-messages')
+            let messagesFromData = await axios.get('https://chatbeta.onrender.com/chat-get-messages')
 
             // https://chatbeta.onrender.com/chat-get-messages
 
